e0d34bb3d8de1007d874a96c651d3c112582113d
Andy Gutsche <andy.gutsche@aperto.com>
2018-09-24 12:12:06 +0200

    Add top and left offset parameter to isInViewport helper

--- src/detection/is-in-viewport.ts
+++ src/detection/is-in-viewport.ts
@@ -4,29 +4,34 @@
  * Check if element is in viewport
  *
  * @param {Object} elem - Object, which we want to check
- * @param {boolean} useBounds - if true, whole element must be visible
+ * @param {Boolean} [useBounds] - if true, whole element must be visible
+ * @param {Number} [topOffsetDelta] - offset to be added to element top offset
+ * @param {Number} [leftOffsetDelta] - offset to be added to element left offset
  *
- * @return {boolean}
+ * @return {Boolean}
  */
-export default function isInViewport(elem: any, useBounds = false): boolean {
-	let el = elem;
-	let top = el.offsetTop;
-	let left = el.offsetLeft;
-	let width = el.offsetWidth;
-	let height = el.offsetHeight;
-	let cond = false;
+export default function isInViewport(elem: any, useBounds = false, topOffsetDelta = 0, leftOffsetDelta = 0): boolean {
+    let el = elem;
+    let top = el.offsetTop;
+    let left = el.offsetLeft;
+    let width = el.offsetWidth;
+    let height = el.offsetHeight;
+    let cond = false;
 
-	while (el.offsetParent) {
-		el = el.offsetParent;
-		top += el.offsetTop;
-		left += el.offsetLeft;
-	}
+    while (el.offsetParent) {
+        el = el.offsetParent;
+        top += el.offsetTop;
+        left += el.offsetLeft;
+    }
 
-	if (useBounds) {
-		cond = top >= window.pageYOffset && left >= window.pageXOffset && (top + height) <= (window.pageYOffset + window.innerHeight) && (left + width) <= (window.pageXOffset + window.innerWidth);
-	} else {
-		cond = top < (window.pageYOffset + window.innerHeight) && left < (window.pageXOffset + window.innerWidth) && (top + height) > window.pageYOffset && (left + width) > window.pageXOffset;
-	}
+    top += topOffsetDelta;
+    left += leftOffsetDelta;
 
-	return cond;
+    if (useBounds) {
+        cond = top >= window.pageYOffset && left >= window.pageXOffset && (top + height) <= (window.pageYOffset + window.innerHeight) && (left + width) <= (window.pageXOffset + window.innerWidth);
+    } else {
+        cond = top < (window.pageYOffset + window.innerHeight) && left < (window.pageXOffset + window.innerWidth) && (top + height) > window.pageYOffset && (left + width) > window.pageXOffset;
+    }
+
+    return cond;
 };
